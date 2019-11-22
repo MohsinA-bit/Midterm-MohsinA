8ef4d4a0d72b178ac22e9c1ca9b619efacdd6ba0
MohsinA-bit <56942181+MohsinA-bit@users.noreply.github.com>
2019-11-22 18:43:25 -0500

    Add files via upload

---
+++ Midtem-IT.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Midterm_Ali
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 12; Boolean keepLooping = true; while (keepLooping)
+            {
+                if (i <= 11) keepLooping = false; i++; Console.WriteLine(i);
+                {
+                    int loop = 2;
+                    while (loop < 128)
+                    {
+                        Console.WriteLine("[" + loop + "]");
+                        loop = loop + loop;
+                    }
+                    {
+                        bool keepLoopimg = true;
+
+                        while (keepLoopimg)
+                        {
+                            Console.WriteLine("this will never end");
+                        }
+                        {
+                            for (int i = 49; i > 0; i--)
+
+                                Console.Write(i);
+                            if (i != 1)
+                            {
+                                Console.Write(",");
+                            }
+                           
+                           {
+                                int i = 1;
+                                while (i <= 21)
+                                    
+                                {
+                                    if (i % 2 != 0)
+                                    {
+                                        Console.Write(i + "");
+                                        i++;
+                                    }
+                                    else
+                                    {
+
+                                    }
+                                    {
+                                        bool icyRain = false, tornadoWarning = false;
+
+                                        if (icyRain == false && tornadoWarning == false)
+                                        {
+                                            Console.WriteLine("lets go outside");
+
+                                        }
+                                    }
+
+
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+//question 1. A For Loop executes if the condition is true and and terminates when the condition is false.
+//  If statement- the statement executes if its true and skips the statement if its false.
+// While Loop- the loop checks the statement to continue to make sure it is true and it keeps repeating.
+
+//Question 6. The do while loop only outputs a single "*" and the while loop doesn't output any. The while loop will
+// check the condition before executing the program and the do while will execute the program then check the condition
+// after.
+
+//Question 7. You can combine true and false values by using the boolean operators, which take true and false values
+// as operands and compute new values. If statement completed right before comments start.
+
+
+        
+
+            
+
+
+            
+    
+
